13c37c7f55a236ff494a8a2f8d7254d2679af2ce
root <info@silksoft.org>
2022-08-27 12:44:04 -0700

    [FIX] Making sure stock_valuation_layer is updated with new accounting date

--- models/inv_adj_date.py
+++ models/inv_adj_date.py
@@ -11,8 +11,8 @@ class InventoryAdjustmentDate(models.Model):
         res = super(StockQuant, self)
         move_vals = []
         inventories = self.env['stock.quant']
-        test = self.accounting_date
-        super(InventoryAdjustmentDate, inventories.with_context(force_period_date=test))._apply_inventory()
+        main_date = self.accounting_date
+        super(InventoryAdjustmentDate, inventories.with_context(force_period_date=main_date))._apply_inventory()
         if not self.user_has_groups('stock.group_stock_manager'):
             raise UserError(_('Only a stock manager can validate an inventory adjustment.'))
         for quant in self:
@@ -29,16 +29,23 @@ class InventoryAdjustmentDate(models.Model):
                                                      quant.product_id.with_company(quant.company_id).property_stock_inventory,
                                                      out=True))
         moves = self.env['stock.move'].with_context(inventory_mode=False).create(move_vals)
-        moves.with_context(force_period_date=test)._action_done()
+        moves.with_context(force_period_date=main_date)._action_done()
         self.location_id.write({'last_inventory_date': quant.accounting_date})
         date_by_location = {loc: loc._get_next_inventory_date() for loc in self.mapped('location_id')}
         for quant in self:
             # quant.date = quant.accounting_date
             quant.inventory_date = date_by_location[quant.location_id]
         move = self.env['stock.move'].search([('id', '=', moves.id)])
-        move.date = test
+        move.date = main_date
         for move_lines in move.move_line_ids:
             move_lines.date = quant.accounting_date
+        for moves in move:
+            self.env.cr.execute(
+                "UPDATE stock_valuation_layer SET create_date = (%s) WHERE stock_move_id = %s",
+                [main_date, int(moves.id)])
+            # stock_valuation = self.env['stock.valuation.layer'].search([('stock_move_id', '=', moves.id)])
+            # for stocks in stock_valuation:
+            #     stocks.create_date = main_date
         self.write({'inventory_quantity': 0, 'user_id': False})
         self.write({'inventory_diff_quantity': 0})
         self.write({'accounting_date': 0})
